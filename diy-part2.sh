5560c08fb301edcb12eff545441a3ac6d84fa5cc
lollypop2018 <35105460+lollypop2018@users.noreply.github.com>
2024-05-05 20:42:34 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,13 +11,13 @@
 #
 
 # 修改openwrt登陆地址,把下面的 192.168.50.5 修改成你想要的就可以了
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # 修改主机名字，把 iStore OS 修改你喜欢的就行（不能纯数字或者使用中文）
-#sed -i 's/OpenWrt/LOLLYPOP-Router/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/LOLLYPOP-Router/g' package/base-files/files/bin/config_generate
 
 # 添加自定义软件包
 # echo '
